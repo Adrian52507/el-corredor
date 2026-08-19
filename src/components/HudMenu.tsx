9f46759bb1ad0b760643fec7bb8e7f554c29865f
Adrian <adrian52507@gmail.com>
2026-08-19 00:29:35 -0500

    menu interactivo basico

--- src/components/HudMenu.tsx
+++ src/components/HudMenu.tsx
@@ -1,10 +1,22 @@
-import { stops } from '../data/stops'
+import { useState } from 'react'
+import { stops, type StopId } from '../data/stops'
 
 export default function HudMenu() {
+  const [activeStop, setActiveStop] = useState<StopId>('inicio')
+
   return (
     <ul>
       {stops.map((stop) => (
-        <li key={stop.id}>{stop.label}</li>
+        <li
+          key={stop.id}
+          onClick={() => setActiveStop(stop.id)}
+          style={{
+            fontWeight: activeStop === stop.id ? 'bold' : 'normal',
+            cursor: 'pointer',
+          }}
+        >
+          {stop.label}
+        </li>
       ))}
     </ul>
   )
